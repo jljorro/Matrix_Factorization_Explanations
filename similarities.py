d2efdf99c1eda4495056ce55e66c53c350fb5e2b
jljorro <jljorro@ucm.es>
2019-03-13 15:05:45 +0100

    Calculado el margen de error entre rating calculado y rating real

--- similarities.py
+++ similarities.py
@@ -1,3 +1,30 @@
+import pandas as pd
+
+from scipy.spatial.distance import pdist, squareform
+from scipy.spatial.distance import euclidean, cosine, cityblock
+
+
+def get_binary_similarity_matrix():
+    binary_prop = pd.read_csv('data/experiment_data/movies_binary.csv')
+    
+    movies_prop = binary_prop.drop(columns=['id']).values
+    movies_sim_values = pdist(movies_prop, equal_sim)
+    movies_similarity = pd.DataFrame(squareform(movies_sim_values))
+    
+    return movies_similarity
+
+
+def get_similarity_matrix(user, similarity_function):    
+    q_path = 'data/matrices_data/q_user_{}.csv'.format(user)
+    q_u = pd.read_csv(q_path)
+    
+    movies_prop = q_u.drop(columns=['movieId']).values
+    movies_sim_values = pdist(movies_prop, similarity_function)
+    movies_similarity = pd.DataFrame(squareform(movies_sim_values))
+    
+    return movies_similarity
+
+
 # Creamos la función de similitud
 # ¡OJO! Para evitar similitudes muy altas (debido a la cantidad de ceros)
 # solo contamos, para la similitud, las propiedades que tengan valor 1 en
@@ -11,3 +38,15 @@ def equal_sim(item1, item2):
         if item1[i] == 1 or item2[i] == 1:
             atr = atr + 1
     return float(atr - dif)/float(atr)
+
+
+def euclidean_sim(item1, item2):
+    return 1 / (1 + euclidean(item1, item2))
+
+
+def cosine_sim(item1, item2):
+    return 1 / (1 + cosine(item1, item2))
+
+
+def manhattan_sim(item1, item2):
+    return 1 / (1 + cityblock(item1, item2))
--- utils.py
+++ utils.py
@@ -1,5 +1,90 @@
 import pandas as pd
 import numpy as np
+import statistics
+
+def rating_dif(row):
+    """
+        Funcion que crea una columna con las diferencias entre los ratings reales del item y los ratings de los items
+        similares
+    """
+    #print(row)
+    differences = [(abs(row['real_rating'] - row[dim])) for dim in range(len(row) - 1)]
+    #return statistics.mean(differences)
+    return differences
+
+def rating_mean(row):
+    """
+        Funcion que crea una columna con la media de las diferencias
+    """
+    #print(row['differences'])
+    if 'differences' in row.index:
+        return statistics.mean(row['differences'])
+    else:
+        return 0.0
+
+
+def rating_pstdev(row):
+    """
+        Funcion que crea una columna con la desviacion tipica de las diferencias
+    """
+    if 'differences' in row.index:
+        return statistics.pstdev(row['differences'])
+    else:
+        return 0
+
+
+def calculate_mean_ratings(sim_DF, test_movies, user, k):
+    """
+        Funcion que calcula la media de la diferencias de los ratings 
+    """
+    
+    # Get train data by user
+    trainset_DF = pd.read_csv('data/experiment_data/trainset.csv')
+    train_user_data = trainset_DF[trainset_DF.userId == user]
+    
+    # Get test data by user
+    testset_DF = pd.read_csv('data/experiment_data/testset.csv')
+    test_user_data = testset_DF[testset_DF.userId == user]
+
+    if len(test_user_data) > 0:
+        # De cada película del test obtenemos los K más similares y nos quedamos con sus ratings
+        predicted_ratings = {}
+        for i in range(len(test_movies)):
+            most_similar_movies = sim_DF.iloc[i].sort_values(ascending=False).index.values[:k]
+            rating_predicted = train_user_data[train_user_data.movieId.isin(most_similar_movies)].rating.values
+            predicted_ratings[test_movies[i]] = rating_predicted
+
+        predicted_ratings_DF = pd.DataFrame.from_dict(predicted_ratings, orient='index')
+        predicted_ratings_DF['real_rating'] = test_user_data.rating.values    
+
+        # Calculamos la media de diferencias de cada rating
+        predicted_ratings_DF['differences'] = predicted_ratings_DF.apply(lambda row: rating_dif(row), axis=1)
+        predicted_ratings_DF['mean'] = predicted_ratings_DF.apply(lambda row: rating_mean(row), axis=1)
+        #predicted_ratings_DF['pstdev'] = predicted_ratings_DF.apply(lambda row: rating_pstdev(row), axis=1)
+
+        # media total
+        mean_total = statistics.mean(predicted_ratings_DF['mean'].tolist())
+
+        return mean_total
+    else:
+        return 0.0
+
+def get_all_movies(user):
+    q_path = 'data/matrices_data/q_user_{}.csv'.format(user)
+    q_u = pd.read_csv(q_path)
+    return q_u.movieId.values
+
+def get_train_test_movies(user):
+    trainset_DF = pd.read_csv('data/experiment_data/trainset.csv')
+    train_user_data = trainset_DF[trainset_DF.userId == user]
+    
+    testset_DF = pd.read_csv('data/experiment_data/testset.csv')
+    test_user_data = testset_DF[testset_DF.userId == user]
+    
+    test_movies = test_user_data.movieId.values
+    train_movies = train_user_data.movieId.values
+    
+    return train_movies, test_movies
 
 def filter_similarity_matrix(dataframe, test_movies, train_movies, movies_ids):
     # Ponemos los id de las películas en las filas de similitud
